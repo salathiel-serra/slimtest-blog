3423b33c42f548e00a2ed4b9c3ad9765c677990d
Salathiel Serra <thiel.serra@gmail.com>
2022-06-30 17:43:22 -0300

    Refatorando controllers

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,8 +9,9 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new Slim\App();
 
 $container = $app->getContainer();
+
 $container['HomeController'] = function($container){
-    return new App\Controllers\HomeController;
+    return new App\Controllers\HomeController($container);
 };
 
 require __DIR__ . '/routes.php';
