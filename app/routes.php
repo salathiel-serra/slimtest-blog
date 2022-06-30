0c18f26b19d67c31abba45b2a12963f08bd5c7c4
Salathiel Serra <thiel.serra@gmail.com>
2022-06-30 19:08:22 -0300

    Agrupando rotas

--- app/routes.php
+++ app/routes.php
@@ -2,5 +2,8 @@
 
 $app->get('/', 'HomeController:index')->setName('home');
 
-$app->get('/login', 'AuthController:login')->setName('auth.login');
-$app->get('/register', 'AuthController:register')->setName('auth.register');
+
+$app->group('/auth', function($app){
+    $app->map(['GET', 'POST'], '/login', 'AuthController:login')->setName('auth.login');
+    $app->map(['GET', 'POST'], '/register', 'AuthController:register')->setName('auth.register');
+});
