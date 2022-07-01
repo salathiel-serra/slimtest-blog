507323b8c15b0a7175e4ae42b4dc90b412296543
Salathiel Serra <thiel.serra@gmail.com>
2022-07-01 17:34:05 -0300

    Registrando usuário

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -14,5 +15,21 @@ class AuthController extends Controller
     {
         if($request->isGet())
             return $this->container->view->render($response, 'register.twig');
+
+
+        $confirmationKey = bin2hex(random_bytes(20));
+
+        $expirationDate  = new \DateTime(date('d/m/Y H:i:s'));
+        $expirationDate->modify('+1 hour');
+
+        User::create([
+            'name'                  => $request->getParam('name'),
+            'email'                 => $request->getParam('email'),
+            'password'              => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
+            'confirmation_key'      => $confirmationKey,
+            'confirmation_expires'  => $expirationDate
+        ]);
+
+        return $response->withRedirect( $this->container->router->pathFor('auth.login') );
     }
 }
